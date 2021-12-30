7d4275e43ed7983480fec03c4e7002a284a25d37
托噜酱 <58923223+tonylu110@users.noreply.github.com>
2021-12-30 17:14:59 +0800

    0.1.0 beta version

--- newPage/js/features/hide.js
+++ newPage/js/features/hide.js
@@ -1,30 +1,4 @@
 function hideUse(){
-    //This is show all element function
-    function onShow(){
-        elemenetGetId('mainlink').style.display=""; //show link element
-        elemenetGetId('button').hidden = false; //show bottom button
-        elemenetGetId('history').hidden = false; //show history button
-        elemenetGetId('reload').hidden = false; //show reload button
-        elemenetGetId('info').hidden = false; //show info button
-        elemenetGetId('cal').hidden = false; // show calculator button
-        elemenetGetId('hide').hidden = false; // show hide button
-        elemenetGetId('searchbar').style.display=""; //show searchBar button
-        elemenetGetId('clock').hidden = false; //show clock button
-    }
-
-    //This is hide all element function
-    function onHide(){
-        elemenetGetId("mainlink").style.display="none"; //hide link elemnet
-        elemenetGetId('button').hidden = true; //hide bottom button
-        elemenetGetId('history').hidden = true; //hide history button
-        elemenetGetId('reload').hidden = true; //hide reload button
-        elemenetGetId('info').hidden = true; //hide info button
-        elemenetGetId('cal').hidden = true; //hide calculator button
-        elemenetGetId('hide').hidden = true; //hide hide button
-        elemenetGetId('searchbar').style.display="none"; //hide searchBar button
-        elemenetGetId('clock').hidden = true; //hide clock button
-    }
-
     //This is hide all element function
     //if bottemButton is hide all element will hide
     //else show all element
@@ -52,31 +26,6 @@ function hideUse(){
 }
 
 function hs(){
-    //This is show all element function
-    function onShow(){
-        elemenetGetId('mainlink').style.display=""; //show link element
-        elemenetGetId('button').hidden = false; //show bottom button
-        elemenetGetId('history').hidden = false; //show history button
-        elemenetGetId('reload').hidden = false; //show reload button
-        elemenetGetId('info').hidden = false; //show info button
-        elemenetGetId('cal').hidden = false; // show calculator button
-        elemenetGetId('hide').hidden = false; // show hide button
-        elemenetGetId('searchbar').style.display=""; //show searchBar button
-        elemenetGetId('clock').hidden = false; //show clock button
-    }
-
-    //This is hide all element function
-    function onHide(){
-        elemenetGetId("mainlink").style.display="none"; //hide link elemnet
-        elemenetGetId('button').hidden = true; //hide bottom button
-        elemenetGetId('history').hidden = true; //hide history button
-        elemenetGetId('reload').hidden = true; //hide reload button
-        elemenetGetId('info').hidden = true; //hide info button
-        elemenetGetId('cal').hidden = true; //hide calculator button
-        elemenetGetId('hide').hidden = true; //hide hide button
-        elemenetGetId('searchbar').style.display="none"; //hide searchBar button
-        elemenetGetId('clock').hidden = true; //hide clock button
-    }
     var screenWidth = body().offsetWidth;
     chrome.storage.sync.get(['hs'],function(budget){
         if(budget.hs == 'show'){
--- newPage/js/main/main.js
+++ newPage/js/main/main.js
@@ -33,6 +33,33 @@ function changeStyle(){
   }
 }
 
+//This is show all element function
+function onShow(){
+  elemenetGetId('mainlink').style.display=""; //show link element
+  elemenetGetId('button').hidden = false; //show bottom button
+  elemenetGetId('history').hidden = false; //show history button
+  elemenetGetId('reload').hidden = false; //show reload button
+  elemenetGetId('info').hidden = false; //show info button
+  elemenetGetId('cal').hidden = false; // show calculator button
+  elemenetGetId('hide').hidden = false; // show hide button
+  elemenetGetId('searchbar').style.display=""; //show searchBar button
+  elemenetGetId('clock').hidden = false; //show clock button
+}
+
+//This is hide all element function
+function onHide(){
+  elemenetGetId("mainlink").style.display="none"; //hide link elemnet
+  elemenetGetId('button').hidden = true; //hide bottom button
+  elemenetGetId('history').hidden = true; //hide history button
+  elemenetGetId('reload').hidden = true; //hide reload button
+  elemenetGetId('info').hidden = true; //hide info button
+  elemenetGetId('cal').hidden = true; //hide calculator button
+  elemenetGetId('hide').hidden = true; //hide hide button
+  elemenetGetId('searchbar').style.display="none"; //hide searchBar button
+  elemenetGetId('clock').hidden = true; //hide clock button
+}
+
+
 function mainUse(){
   //This is onload script
   //test 'https://api.iyk0.com/ecy/api.php' can or not work
@@ -48,32 +75,6 @@ function mainUse(){
       print('network is not working');
       elemenetGetId('background').style.backgroundImage = "url('imgs/background - " + Math.ceil(Math.random()*10) + ".jpg')";
   };
-
-  //This is show all element function
-  function onShow(){
-    elemenetGetId('mainlink').style.display=""; //show link element
-    elemenetGetId('button').hidden = false; //show bottom button
-    elemenetGetId('history').hidden = false; //show history button
-    elemenetGetId('reload').hidden = false; //show reload button
-    elemenetGetId('info').hidden = false; //show info button
-    elemenetGetId('cal').hidden = false; // show calculator button
-    elemenetGetId('hide').hidden = false; // show hide button
-    elemenetGetId('searchbar').style.display=""; //show searchBar button
-    elemenetGetId('clock').hidden = false; //show clock button
-  }
-
-  //This is hide all element function
-  function onHide(){
-    elemenetGetId("mainlink").style.display="none"; //hide link elemnet
-    elemenetGetId('button').hidden = true; //hide bottom button
-    elemenetGetId('history').hidden = true; //hide history button
-    elemenetGetId('reload').hidden = true; //hide reload button
-    elemenetGetId('info').hidden = true; //hide info button
-    elemenetGetId('cal').hidden = true; //hide calculator button
-    elemenetGetId('hide').hidden = true; //hide hide button
-    elemenetGetId('searchbar').style.display="none"; //hide searchBar button
-    elemenetGetId('clock').hidden = true; //hide clock button
-  }
 
   //This is historybar function
   elemenetGetId('history').onclick = function onBarButton(){

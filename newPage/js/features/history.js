567fc01b6bcb4f42d28109466b9311acde99a5c2
托噜酱 <58923223+tonylu110@users.noreply.github.com>
2022-05-26 10:43:00 +0800

    1.2.7 version

--- newPage/js/features/history.js
+++ newPage/js/features/history.js
@@ -1,4 +1,4 @@
-import { elemenetGetId, elemenetGetClass } from "../main/function.js";
+import { elemenetGetId } from "../main/function.js";
 
 export default function historyUse() {
     var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
@@ -8,21 +8,33 @@ export default function historyUse() {
         'startTime': oneWeekAgo,
         'maxResults': 50
     };
-    chrome.history.search(query, function (results) {
-        for (i = 0; i < results.length; i++) {
+    chrome.history.search(query, (results) => {
+        for (var i = 0; i < results.length; i++) {
             var title = results[i].title;
             var link = results[i].url;
-            var html = '<div class="historyInfo"><div class="historyTitle">' + title + '</div>' + '<div id="historyLink' + i + '" class="historyLink">' + link + '</div></div>';
-            elemenetGetId('historyIn').innerHTML += html;
-        }
-        var historyInfo = elemenetGetClass('historyInfo');
-        for (var i = 0; i < historyInfo.length; i++) {
-            historyInfo[i].index = i;
-            historyInfo[i].link = elemenetGetId('historyLink' + i).innerHTML;
-            historyInfo[i].onclick = function () {
+            /**
+             * create element like this:
+             *  <div class="historyInfo">
+                    <div class="historyTitle">title</div>
+                    <div class="historyLink">link</div>
+                </div>
+             */
+            let historyInfo = document.createElement('div');
+            let historyTitle = document.createElement('div');
+            let historyLink = document.createElement('div');
+            historyInfo.className = 'historyInfo';
+            historyTitle.className = 'historyTitle';
+            historyTitle.innerText = title;
+            historyLink.className = 'historyLink';
+            historyLink.innerText = link;
+            historyInfo.appendChild(historyTitle);
+            historyInfo.appendChild(historyLink);
+            elemenetGetId('historyIn').appendChild(historyInfo);
+            // open history link in new tab
+            historyInfo.onclick = () => {
                 chrome.tabs.create({
-                    url: this.link
-                });
+                    url: historyLink.innerText
+                })
             }
         }
     });

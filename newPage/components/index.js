2e6275339810543988183dc4ef29fcb40bcf930f
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-10-06 20:58:59 +0800

    Add: main links component

--- newPage/components/index.js
+++ newPage/components/index.js
@@ -1,5 +1,7 @@
 import searchBar from "./mainArea/searchBar.js";
 import moreSearch from "./mainArea/moreSearch.js"
+import links from "./mainArea/links.js";
 
 customElements.define('search-bar', searchBar)
 customElements.define('more-search', moreSearch)
+customElements.define('main-link', links)

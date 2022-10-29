aebd74043ccf3894d9fd472ff921b7778ce7f680
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2022-10-29 19:12:33 +0800

    Refactor

--- newPage/components/settingMain/settingInnerMain/backgroundMain/index.js
+++ newPage/components/settingMain/settingInnerMain/backgroundMain/index.js
@@ -1,6 +1,7 @@
 import apiImgs from './apiImgs.js'
 import exImgs from './exImgs.js'
 import {apiImg, loaclImgChange} from "./imgChange.js";
+import isMobile from "../../../../util/isMobile.js";
 
 export default class backgroundMain extends HTMLElement {
   constructor() {
@@ -40,5 +41,48 @@ export default class backgroundMain extends HTMLElement {
   init() {
     loaclImgChange()
     apiImg()
+    document.getElementById('backgroundUpload').onchange = function () {
+      let file = this.files[0]
+      let reader = new FileReader
+      document.getElementById('exImg').style.display = 'none'
+      reader.readAsDataURL(file)
+      reader.onload = function (e) {
+        document.getElementById('backgroundSetButton').hidden = false
+        document.getElementById('viewImg').style.display = ''
+        document.getElementById('viewImg').hidden = false
+        document.getElementById('viewImg').src = e.target.result
+        if (isMobile()) {
+          document.getElementById('viewImg').style.maxWidth = '130px'
+        }
+        document.getElementById('fileName').innerText = file.name
+        const picUrl = e.target.result
+        document.getElementById('backgroundSetButton').addEventListener("click", () => {
+          chrome.storage.local.set({ 'picUrl': picUrl })
+          location.reload()
+        })
+      }
+    }
+    document.getElementById('backgroundBefore').addEventListener("click", () =>{
+      chrome.storage.local.set({ 'picUrl': '' })
+      location.reload()
+    })
+    document.getElementById('review').addEventListener("click", () => {
+      document.getElementById('viewImg').src = document.getElementById('onlinePicUrl').value
+      document.getElementById('viewImg').addEventListener('error', () =>{
+        document.getElementById('picError').innerText = '请输入正确的图片链接'
+        document.getElementById('picError').hidden = false
+      })
+      document.getElementById('viewImg').addEventListener('load', () => {
+        document.getElementById('exImg').style.display = 'none'
+        document.getElementById('picError').hidden = true
+        document.getElementById('viewImg').style.display = ''
+        document.getElementById('viewImg').hidden = false
+        document.getElementById('backgroundSetButton').hidden = false
+      })
+      document.getElementById('backgroundSetButton').addEventListener("click", () => {
+        chrome.storage.local.set({ 'picUrl': document.getElementById('onlinePicUrl').value })
+        location.reload()
+      })
+    })
   }
 }
--- newPage/js/main/settings.js
+++ newPage/js/main/settings.js
@@ -3,11 +3,9 @@ import { moreSearch } from '../features/search.js';
 import { hsFeature } from '../features/hsFeature.js';
 import { hsLink } from '../features/hsLink.js';
 import { changeLink, useChangeLink } from '../features/changeLink.js';
-import { background } from '../features/backgroud.js'
 
 export default function settingUse() {
     //other setting feature
-    background(); //setting background
     hsFeature(); //hide feature
     hsLink();//hide link
     changeLink();//change link
